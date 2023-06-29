5ff7069f364d067b91c7c377ce82f341e7fcec85
Vasiliy Batanov <wisakejjak@gmail.com>
2023-06-29 21:39:35 +0300

    Added quick sort

--- sort/quick_sort/quick_sort.c
+++ sort/quick_sort/quick_sort.c
@@ -1,13 +1,54 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <climits>
 
-int quick_sort(int* pA, int N, int x)
-{
-    
-
-    return -1;
+void swap(int *firstNumber, int *secondNumber){
+    int tmp = *firstNumber;
+    *firstNumber = *secondNumber;
+    *secondNumber = tmp;
 }
 
+void quick_sort(int* pArray, int left, int right){
+    if (left < right){
+        int pivotIndex = floor((left+right)/2);
+        int pivot = pArray[pivotIndex];
+        swap(&pArray[pivotIndex], &pArray[right]);
+        int i = left;
+        for (int j = left; j < right; j++){
+            if(pArray[j] < pivot){
+                swap(&pArray[i], &pArray[j]);
+                i++;
+            }
+        }
+        swap(&pArray[i], &pArray[right]);
+        quick_sort(pArray, left, i-1);
+        quick_sort(pArray, i+1, right);
+    }
+}
+
+
 int main (void){
-    int* pA = mall
+    printf("Size of array from 1 to %i:\n", INT_MAX);
+    int arraySize;
+    scanf("%d", &arraySize);
+    int* pArray = (int*)malloc(arraySize*sizeof(int));
+    for(int i = 0; i < arraySize; i++){
+        pArray[i] = rand() % INT_MAX-1;
+    }
+    
+    for(int i = 0; i < arraySize; i++){
+        printf("%i ",pArray[i]);
+        if (i % 10 == 0) printf("\n");
+    }
+    
+    quick_sort(pArray, 0, arraySize-1);
+    
+    for(int i = 0; i < arraySize; i++){
+        printf("%i ",pArray[i]);
+        if (i % 10 == 0) printf("\n");
+    }
+    
+    free(pArray);
     return 0;
 }
