bc0bb8368d7510cb8a070e6324805d19ed048aab
Batanov Vasiliy <77393101+wisakejjak@users.noreply.github.com>
2023-07-20 17:54:17 +0300

    Update binary_search.cpp

--- search/binary_search/binary_search.cpp
+++ search/binary_search/binary_search.cpp
@@ -3,18 +3,15 @@
 #include <climits>
 //pA - pointer to array, N - size of array, x - desired number
 int binary_search (int* pA, int N, int x){ 
-	int left_side = 0;
-    int right_side = N - 1;
-    int middle = 0;
-    while (left_side <= right_side)
-    {
-        middle = floor((left_side + right_side)/2);
-        if (pA[middle] < x)
-        {
+    int left_side = 0;// left border of array
+    int right_side = N - 1;// right border of array
+    int middle = 0;// middle of array = 0
+    while (left_side <= right_side){
+        middle = floor((left_side + right_side)/2);// real middle of array
+        if (pA[middle] < x){//searching on left side
             left_side = middle + 1;
         }
-        else if (pA[middle] > x)
-        {
+        else if (pA[middle] > x){//searching on right side
             right_side = middle - 1;
         }
         else return middle;
@@ -25,17 +22,17 @@ int binary_search (int* pA, int N, int x){
 int main (){
 	std::cout << "Size of array from 1 to " << INT_MAX << ": "<< std::endl;
 	int N;
-	std::cin >> N;
+	std::cin >> N;//size of array
 	std::cout << "Number to search from 0 to " << INT_MAX-1 <<": " << std::endl;
 	int x;
-	std::cin >> x;
-	int* pA = new int[N];
-	for (int i = 0; i < N; i++)
+	std::cin >> x;//searching number
+	int* pA = new int[N];// allocate the memory
+	for (int i = 0; i < N; i++)//array filling
 		pA[i] = i;
-	int answer = binary_search (pA, N, x);
+	int answer = binary_search (pA, N, x);//binary search
 	((answer < N)&&(answer >= 0)) 
                             ? std::cout << "A[" << answer << "] = " << x << std::endl
-	                        : std::cout << x << " is not in array" << std::endl;
-	delete[] pA;
+	                        : std::cout << x << " is not in array" << std::endl;//answer print
+	delete[] pA;//free the memory
 	return 0;
 }
