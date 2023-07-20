cb11cb94c69992e8e27917b90dc90d646ad88ffa
Batanov Vasiliy <77393101+wisakejjak@users.noreply.github.com>
2023-07-20 18:49:15 +0300

    Update linear_search.c

--- search/linear_search/linear_search.c
+++ search/linear_search/linear_search.c
@@ -6,7 +6,7 @@ int linear_search (int* pA, int N, int x){ //linear search
 				    //pA - pointer to array
 				    //N - size of array
 				    //x - desired number
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < N; i++) //searching
 		if (pA[i] == x)
 			return i;
 	return -1;
@@ -15,17 +15,17 @@ int linear_search (int* pA, int N, int x){ //linear search
 int main (void){
 	printf("Size of array from 1 to %i:\n", INT_MAX);
 	int N;
-	scanf("%d", &N);
+	scanf("%d", &N);//size of array
 	printf("Number to search from 0 to %i:\n", INT_MAX-1);
 	int x;
-	scanf("%d", &x);
-	int* pA = (int*) malloc(N*sizeof(int));
-	for (int i = 0; i < N; i++){
+	scanf("%d", &x);//searching array
+	int* pA = (int*) malloc(N*sizeof(int));//allocate the memory
+	for (int i = 0; i < N; i++){//array filling (random number from 0 to INT_MAX-1)
 		pA[i] = rand() % INT_MAX-1;
 	}
-	int answer = linear_search (pA, N, x);
+	int answer = linear_search (pA, N, x);//linear search
 	(answer != -1) ? printf ("nA[%d] = %d\n", answer, x)
-                   : printf ("%d is not in array\n", x);
-	free(pA);
+                   : printf ("%d is not in array\n", x);//print answer
+	free(pA);//free the memory
     	return 0;
 }
