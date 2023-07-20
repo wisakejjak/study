5eef5372549d475e7b4f30a47a9994c30e25dc9d
Batanov Vasiliy <77393101+wisakejjak@users.noreply.github.com>
2023-07-20 18:47:04 +0300

    Update quick_sort.cpp

--- sort/quick_sort/quick_sort.cpp
+++ sort/quick_sort/quick_sort.cpp
@@ -2,17 +2,17 @@
 #include <cmath>
 #include <climits>
 
-void swap(int *firstNumber, int *secondNumber){
+void swap(int *firstNumber, int *secondNumber){//swap function
     int tmp = *firstNumber;
     *firstNumber = *secondNumber;
     *secondNumber = tmp;
 }
 
-void quick_sort(int* pArray, int left, int right){
+void quick_sort(int* pArray, int left, int right){//quick_sort function
     if (left < right){
-        int pivotIndex = floor((left+right)/2);
-        int pivot = pArray[pivotIndex];
-        swap(&pArray[pivotIndex], &pArray[right]);
+        int pivotIndex = floor((left+right)/2);//set index of pivot array
+        int pivot = pArray[pivotIndex];//set element of array by index
+        swap(&pArray[pivotIndex], &pArray[right]);//swap pointers (pivot and right)
         int i = left;
         for (int j = left; j < right; j++){
             if(pArray[j] < pivot){
@@ -21,8 +21,8 @@ void quick_sort(int* pArray, int left, int right){
             }
         }
         swap(&pArray[i], &pArray[right]);
-        quick_sort(pArray, left, i-1);
-        quick_sort(pArray, i+1, right);
+        quick_sort(pArray, left, i-1);//quick sort left part of array
+        quick_sort(pArray, i+1, right);//quick sort right part of array
     }
 }
 
@@ -30,24 +30,24 @@ void quick_sort(int* pArray, int left, int right){
 int main (){
     std::cout << "Size of array from 1 to "<< INT_MAX << " : ";
     int arraySize;
-    std::cin >> arraySize;
-    int* pArray = new int [arraySize];
-    for(int i = 0; i < arraySize; i++){
+    std::cin >> arraySize;//size of array
+    int* pArray = new int [arraySize];//allocate the memory
+    for(int i = 0; i < arraySize; i++){//array filling (random number from 0 to INT_MAX-1)
         pArray[i] = rand() % INT_MAX-1;
     }
 
-    std::cout << "\t\tRaw array" << std::endl; 
+    std::cout << "\t\tRaw array" << std::endl;//print raw array
     for(int i = 0; i < arraySize; i++){
         std::cout << pArray[i] << " ";
     }
     
-    quick_sort(pArray, 0, arraySize-1);
+    quick_sort(pArray, 0, arraySize-1);//quick sort
     
-    std::cout << "\n\t\tSorted array" << std::endl;
+    std::cout << "\n\t\tSorted array" << std::endl;//print sorted array
     for(int i = 0; i < arraySize; i++){
         std::cout  << pArray[i] << " ";
     } 
     
-    delete[] pArray;
+    delete[] pArray;//free the memory
     return 0;
 }
