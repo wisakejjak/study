0927e3247af59c8f4f554dff44022f5700c4d50b
Batanov Vasiliy <wisakejjak@gmail.com>
2023-08-18 17:19:40 +0000

    clean

--- src/c_sort/bubble_sort.c
+++ src/c_sort/bubble_sort.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <ncurses.h>
 
 int bubble_sort(int* pArray, int arraySize){
 	for(int i = 0; i < arraySize - 1; i++){
@@ -16,16 +17,37 @@ int bubble_sort(int* pArray, int arraySize){
 	}
 	return 0;
 }
-
+/*
+ * Need to use printw or mvprintw or another asdfasfd_print_adfasff functions to display 
+ * array bars. For example, A[0] displays on 1st row, A[1] on 2nd etc. Or idk i wanna
+ * sleep. 
 int main(){
-	printf("Size of array from 1 to %i:\n", INT_MAX);
+	initscr();
+	noecho();
+	curs_set(0);
+	printw("Size of array from 1 to %i:\n", INT_MAX);
     int arraySize;
-    scanf("%d", &arraySize); //size of array
+    scanw("%d", &arraySize); //size of array
     int* pArray = (int*)malloc(arraySize*sizeof(int)); //allocate the memory
     for(int i = 0; i < arraySize; i++){ //array filling (random number from 0 to 50)
         pArray[i] = rand() % 50;
     }
-    bubble_sort(pArray, arraySize); //bubble sort
+	for(int i = 0; i < arraySize; i++){
+        mvprintw("1,0,\n[%d][",pArray[i]);
+		for(int j = 0; j < pArray[i]; j++){
+			printw("#");
+		}
+		printw("]");
+    }
+	bubble_sort(pArray, arraySize); //bubble sort
+	for(int i = 0; i < arraySize; i++){
+		printw("\n[");
+		for(int j = 0; j < pArray[i]; j++){
+			printw("#");
+		}
+		printw("]");
+    }
+	endwin();
 	free(pArray); //free the memory
 	return 0;
 }
