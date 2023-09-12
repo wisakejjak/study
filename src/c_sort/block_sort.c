6f9eb6192054ff353fd4e140a89352fcb63bc32c
Batanov Vasiliy <wisakejjak@gmail.com>
2023-09-12 09:42:49 +0300

    Deleted printf in c_sort/block_sort. Minor comment styling

--- src/c_sort/block_sort.c
+++ src/c_sort/block_sort.c
@@ -3,20 +3,19 @@
 #include <stdlib.h>
 #include <limits.h>
 
-int block_sort(int* pArray, int arraySize){
+int block_sort(int* pArray, int arraySize){ // block sorting
 	return 0;
 }
 
 int main(){
-	printf("Size of array from 1 to %i:\n", INT_MAX);
     int arraySize;
-    scanf("%d", &arraySize); //size of array
-    int* pArray = (int*)malloc(arraySize*sizeof(int)); //allocate the memory
-    for(int i = 0; i < arraySize; i++){ //array filling (random number from 0 to 50)
+    scanf("%d", &arraySize); // size of array
+    int* pArray = (int*)malloc(arraySize*sizeof(int)); // allocate the memory
+    for(int i = 0; i < arraySize; i++){ // array filling (random number from 0 to 50)
         pArray[i] = rand() % 50;
     }
-    block_sort(pArray, arraySize); //block sort
-	free(pArray); //free the memory
+    block_sort(pArray, arraySize); // block sort
+	free(pArray); // free the memory
 	return 0;
 }
 
