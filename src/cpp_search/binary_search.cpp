64615d801de7161d9367e77e3dfc479b189ec539
Batanov Vasiliy <wisakejjak@gmail.com>
2023-09-12 09:37:13 +0300

    Deleted std::cout in cpp_search. Minor comment styling

--- src/cpp_search/binary_search.cpp
+++ src/cpp_search/binary_search.cpp
@@ -2,17 +2,17 @@
 #include <cmath>
 #include <climits>
 
-//pA - pointer to array, N - size of array, x - desired number
+// pA - pointer to array, N - size of array, x - desired number
 int binary_search (int* pA, int N, int x){ 
-    int left_side = 0;// left border of array
-    int right_side = N - 1;// right border of array
-    int middle = 0;// middle of array = 0
+    int left_side = 0; // left border of array
+    int right_side = N - 1; // right border of array
+    int middle = 0; // middle of array = 0
     while (left_side <= right_side){
-        middle = floor((left_side + right_side)/2);// real middle of array
-        if (pA[middle] < x){//searching on left side
+        middle = floor((left_side + right_side)/2); // real middle of array
+        if (pA[middle] < x){ // searching on left side
             left_side = middle + 1;
         }
-        else if (pA[middle] > x){//searching on right side
+        else if (pA[middle] > x){ // searching on right side
             right_side = middle - 1;
         }
         else return middle;
@@ -21,19 +21,14 @@ int binary_search (int* pA, int N, int x){
 }
 
 int main (){
-	std::cout << "Size of array from 1 to " << INT_MAX << ": "<< std::endl;
 	int N;
-	std::cin >> N;//size of array
-	std::cout << "Number to search from 0 to " << INT_MAX-1 <<": " << std::endl;
+	std::cin >> N; // size of array
 	int x;
-	std::cin >> x;//searching number
-	int* pA = new int[N];// allocate the memory
-	for (int i = 0; i < N; i++)//array filling
+	std::cin >> x; // searching number
+	int* pA = new int[N]; // allocate the memory
+	for (int i = 0; i < N; i++) // array filling
 		pA[i] = i;
-	int answer = binary_search (pA, N, x);//binary search
-	((answer < N)&&(answer >= 0)) 
-    	? std::cout << "A[" << answer << "] = " << x << std::endl
-	   	: std::cout << x << " is not in array" << std::endl;//answer print
-	delete[] pA;//free the memory
+	// int answer = binary_search (pA, N, x); // binary search
+	delete[] pA; // free the memory
 	return 0;
 }
--- src/cpp_search/linear_search.cpp
+++ src/cpp_search/linear_search.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 #include <climits>
 
-int linear_search (int* pA, int N, int x){ //linear search
-				    //pA - pointer to array
-				    //N - size of array
-				    //x - desired number
-	for (int i = 0; i < N; ++i) //searching
+int linear_search (int* pA, int N, int x){ // linear search
+				    // pA - pointer to array
+				    // N - size of array
+				    // x - desired number
+	for (int i = 0; i < N; ++i) // searching
 		if (pA[i] == x)
 			return i;
 	return -1;
 }
 
 int main (){
-	std::cout << "Size of array from 1 to " << INT_MAX << ": " << std::endl;
 	int N;
-	std::cin >> N;//size of array
-	std::cout << "Number to search from 0 to " << INT_MAX-1 << ": " << std::endl;
+	std::cin >> N; // size of array
 	int x;
-	std::cin >> x;//searching array
-	int* pA = new int[N];//allocate the memory
-	for (int i = 0; i < N; i++)//array filling (random number from 0 to INT_MAX-1)
+	std::cin >> x; // searching array
+	int* pA = new int[N]; // allocate the memory
+	for (int i = 0; i < N; i++) // array filling (random number from 0 to INT_MAX-1)
 		pA[i] = rand() % (INT_MAX-1);
-	int answer = linear_search (pA, N, x);//linear search
-	(answer != -1) ? std::cout << "A[" << answer << "] = " << x << std::endl
-	               : std::cout << x << " is not in array" << std::endl;//print answer
-	delete[] pA;//free the memory
+	int answer = linear_search (pA, N, x); // linear search
+	delete[] pA; // free the memory
 	return 0;
 }
