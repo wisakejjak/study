eb9e34347f30b55ac56b855df6ffd5fe790f60d2
Batanov Vasiliy <wisakejjak@gmail.com>
2023-10-16 20:02:20 +0300

    in-place merge sort added in cpp_sort. Test needed

--- src/cpp_sort/in_place_merge_sort.cpp
+++ src/cpp_sort/in_place_merge_sort.cpp
@@ -2,7 +2,52 @@
 #include <climits>
 #include <iostream>
 
-int in_place_merge_sort(int* pArray, int startArray, int arraySize){
+// NEED TO CHECK THIS CODE (AFTER NCURSES)
+
+void merge(int* pArray, int left, int middle, int right){
+	int i, j, k;
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+    int* LeftArray = new int[n1];
+    int* RightArray = new int[n2];
+    for (i = 0; i < n1; i++)
+        LeftArray[i] = pArray[left + i];
+    for (j = 0; j < n2; j++)
+        RightArray[j] = pArray[middle + 1 + j];
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2) {
+        if (LeftArray[i] <= RightArray[j]) {
+            pArray[k] = LeftArray[i];
+            i++;
+        } else {
+            pArray[k] = RightArray[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        pArray[k] = LeftArray[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        pArray[k] = RightArray[j];
+        j++;
+        k++;
+    }
+    delete[] LeftArray;
+    delete[] RightArray;
+}
+
+int in_place_merge_sort(int* pArray, int left, int right){
+	if (left < right) {
+        int middle = left + (right - left) / 2;
+        in_place_merge_sort(pArray, left, middle);
+        in_place_merge_sort(pArray, middle + 1, right);
+        merge(pArray, left, middle, right);
+    }
 	return 0;
 }
 
